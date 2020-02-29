5fa8eb3efd9f07425e94588b2bff8988b4172cc8
krishnau2 <krishnau2@gmail.com>
2020-02-29 18:17:02 +0530

    Basic layout for audio control section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,25 @@
 import React from "react";
+import {
+  MdRotateLeft,
+  MdRotateRight,
+  MdPauseCircleFilled
+} from "react-icons/md";
 
 const Main = () => {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <div className="audio-control-wrap">
+        <div className="audio-controls">
+          <MdRotateLeft className="rotate-btn" />
+          <MdPauseCircleFilled className="pause-btn" />
+          <MdRotateRight className="rotate-btn" />
+        </div>
+        <button className="play-speed-btn">1.0x</button>
+      </div>
+      <div className="graph-wrap"></div>
+      <div className="transcript-wrap"></div>
+    </div>
+  );
 };
 
 export default Main;
